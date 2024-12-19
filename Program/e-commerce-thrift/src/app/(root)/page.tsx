5c0ba79a74cb4e987ef133454efc27be2c35bf24
arthursaocin <arthursaocin@gmail.com>
2024-12-20 01:58:42 +0700

    menambahkan authentication dengan clerk

---
+++ Program/e-commerce-thrift/src/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function home() {
+  return (
+    <div className='p-4'>
+      <Button variant="destructive"
+      size="lg">
+        Click me
+      </Button>
+    </div>
+  )
+}
