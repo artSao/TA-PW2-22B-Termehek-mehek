b1635ad8baac8e4b2a3b5f9a291de1c0acdae58d
arthursaocin <arthursaocin@gmail.com>
2024-12-20 03:31:02 +0700

    membuat setup page

--- Program/e-commerce-thrift/src/app/(root)/page.tsx
+++ Program/e-commerce-thrift/src/app/(root)/page.tsx
@@ -1,11 +1,18 @@
-import { Button } from '@/components/ui/button'
-import { UserButton } from '@clerk/nextjs'
+"use client"
+
+// import { Button } from '@/components/ui/button'
+import Modal from '@/components/ui/modal'
+// import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
-export default function Home() {
+const setupPage= () => {
   return (
     <div className='p-4'>
-      <UserButton />
+     <Modal title='test title' description='test descrition' isOpen onClose={() => { }}>
+        Children
+      </Modal>
     </div>
   )
 }
+
+export default setupPage
