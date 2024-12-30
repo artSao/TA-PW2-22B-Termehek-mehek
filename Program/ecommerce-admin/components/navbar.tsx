4245108fe38e761f2a8da25306968399cde86aa6
arthursaocin <arthursaocin@gmail.com>
2024-12-31 01:45:12 +0700

    main: membuat navigasi(route) untuk ke page settings

--- Program/ecommerce-admin/components/navbar.tsx
+++ Program/ecommerce-admin/components/navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
               <div>Store Switcher</div>
-              <MainNav />
+              <MainNav className="mx-6"/>
               <div className="ml-auto flex items-center space-x-4"><UserButton /></div>
       </div>
     </div>
