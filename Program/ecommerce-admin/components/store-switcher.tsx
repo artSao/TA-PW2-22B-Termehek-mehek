7820174227c01ac7943182ed1a43a51b287a06fc
arthursaocin <arthursaocin@gmail.com>
2024-12-31 02:54:52 +0700

    membuat component command untuk membuat toko

--- Program/ecommerce-admin/components/store-switcher.tsx
+++ Program/ecommerce-admin/components/store-switcher.tsx
@@ -1,3 +1,78 @@
+// "use client";
+
+// import { Store } from "@prisma/client";
+// import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+// import { useStoreModal } from "@/hooks/use-store-modal";
+// import { useParams, useRouter } from "next/navigation";
+// import { useState } from "react";
+// import { Button } from "./ui/button";
+// import { ChevronsUpDown, Store as StoreIcon } from "lucide-react";
+// import { cn } from "@/lib/utils";
+// import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "./ui/command";
+
+// type PopOverTriggerProps = React.ComponentPropsWithoutRef<
+//   typeof PopoverTrigger
+// >;
+
+// interface StoreSwitcherProps extends PopOverTriggerProps {
+//   items: Store[];
+// }
+
+// const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
+//   const StoreModal = useStoreModal();
+//   const params = useParams();
+//   const router = useRouter();
+
+//   const formattedItems = items.map((item) => ({
+//     label: item.name,
+//     value: item.id,
+//   }));
+
+//   const currentStore = formattedItems.find(
+//     (item) => item.value === params.storeId
+//   );
+
+//   const [open, setOpen] = useState(false);
+
+//   const onStoreSelect = (store: { value: string; label: string }) => {
+//     setOpen(false);
+//     router.push(`/${store.value}`);
+//   };
+
+//   return (
+//     <Popover open={open} onOpenChange={setOpen}>
+//       <PopoverTrigger asChild>
+//         <Button
+//           variant="outline"
+//           size="sm"
+//           role="combobox"
+//           aria-expanded={open}
+//           aria-label="Pilih Toko"
+//           className={cn("w-[200px] justify-between", className)}
+//         >
+//           <StoreIcon className="mr-2 h-4 w-4" />
+//           Current Store
+//           <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
+//           <PopoverContent className="w-[200px] p-0">
+//             <Command>
+//               <CommandList>
+//                               <CommandInput placeholder="cari Toko" />
+//                               <CommandEmpty>
+//                                   Toko Tidak Ditemukan
+//                               </CommandEmpty>
+//                               <CommandGroup heading="Toko">{formattedItems.map((store) => (
+//                                   <CommandItem key={store.value} onSelect={() => onStoreSelect(store)} className="text-sm"></CommandItem>
+//                               ))</CommandGroup>
+//               </CommandList>
+//             </Command>
+//           </PopoverContent>
+//         </Button>
+//       </PopoverTrigger>
+//     </Popover>
+//   );
+// };
+
+// export default StoreSwitcher;
 "use client";
 
 import { Store } from "@prisma/client";
@@ -6,9 +81,17 @@ import { useStoreModal } from "@/hooks/use-store-modal";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { Button } from "./ui/button";
-import { ChevronsUpDown, Store as StoreIcon } from "lucide-react";
+import { Check, ChevronsUpDown, PlusCircle, Store as StoreIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Command, CommandInput, CommandList } from "./ui/command";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/command";
+import { CommandSeparator } from "cmdk";
 
 type PopOverTriggerProps = React.ComponentPropsWithoutRef<
   typeof PopoverTrigger
@@ -29,7 +112,7 @@ const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
   }));
 
   const currentStore = formattedItems.find(
-    (item) => item.value === params.storeId
+    (item) => item.value === params?.storeId
   );
 
   const [open, setOpen] = useState(false);
@@ -50,18 +133,51 @@ const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
           aria-label="Pilih Toko"
           className={cn("w-[200px] justify-between", className)}
         >
-                  <StoreIcon className="mr-2 h-4 w-4" />
-                  Current Store
-                  <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
-                  <PopoverContent>
-                      <Command>
-                          <CommandList>
-                              <CommandInput placeholder="cari Toko"/>
-                          </CommandList>
-                      </Command>
-                  </PopoverContent>
+          <StoreIcon className="mr-2 h-4 w-4" />
+          {currentStore?.label || "Pilih Toko"}
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandInput placeholder="Cari Toko" />
+            <CommandEmpty>Toko Tidak Ditemukan</CommandEmpty>
+            <CommandGroup heading="Toko">
+              {formattedItems.map((store) => (
+                <CommandItem
+                  key={store.value}
+                  onSelect={() => onStoreSelect(store)}
+                  className="text-sm"
+                >
+                  <StoreIcon className="mr-4 h-4 w-4" />
+                  {store.label}
+                  <Check
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      currentStore?.value === store.value
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+                  </CommandList>
+                  <CommandSeparator />
+                  <CommandList>
+                      <CommandGroup>
+                          <CommandItem onSelect={() => {
+                              setOpen(false)
+                              StoreModal.onOpen()
+                          }}>
+                              <PlusCircle className="mr-2 h-5 w-5" />
+                              Buat Toko
+                          </CommandItem>
+                      </CommandGroup>
+                  </CommandList>
+        </Command>
+      </PopoverContent>
     </Popover>
   );
 };
