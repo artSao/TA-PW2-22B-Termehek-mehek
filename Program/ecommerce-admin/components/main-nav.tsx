069b16273918be4530134f6685d449a51dad7c47
arthursaocin <arthursaocin@gmail.com>
2024-12-31 00:57:28 +0700

    membuat page untuk Dashboard

---
+++ Program/ecommerce-admin/components/main-nav.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from "react";
+
+export function MainNav({
+    className,
+    ...props
+}: React.HTMLAttributes<HTMLElement>) {
+    return (
+        <nav>
+            This is Main Nav
+        </nav>
+    )
+}
--- Program/ecommerce-admin/components/navbar.tsx
+++ Program/ecommerce-admin/components/navbar.tsx
@@ -1,7 +1,16 @@
-const Navbar = () => {
-    return (
-        <div>Ini Komponen Navbar</div>
-    )
-}
+import { UserButton } from "@clerk/nextjs";
+import { MainNav } from "./main-nav";
 
-export default Navbar
+const Navbar = () => {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+              <div>Store Switcher</div>
+              <MainNav />
+              <div className="ml-auto flex items-center space-x-4"><UserButton /></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
