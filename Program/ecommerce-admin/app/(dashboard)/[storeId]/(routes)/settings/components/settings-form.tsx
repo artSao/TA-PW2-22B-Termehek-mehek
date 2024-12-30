c3ef0c67a514ac710e7abfce9f9b9eea9364d3cd
arthursaocin <arthursaocin@gmail.com>
2024-12-31 04:44:58 +0700

    Settings Update: Membuat form untuk mengubah nama toko (params diambil dari list Toko yang sudah kita buat) dan menambahkan tombol save

--- Program/ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ Program/ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,15 +1,46 @@
 "use client";
 
+import * as z from "zod";
+
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface settingPageProps {
   initialData: Store;
 }
 
+const formSchema = z.object({ name: z.string().min(1) });
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
 export const SettingsForm: React.FC<settingPageProps> = ({ initialData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    console.log(data);
+  };
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -17,8 +48,37 @@ export const SettingsForm: React.FC<settingPageProps> = ({ initialData }) => {
         <Button variant="destructive" size="sm" onClick={() => {}}>
           <Trash className="h-4 2-4" />
         </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nama</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="nama Toko"
+                      disabled={loading}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </div>
-          
+          <Button disabled={loading} type="submit">
+            Save
+          </Button>
+        </form>
+      </Form>
     </>
   );
 };
