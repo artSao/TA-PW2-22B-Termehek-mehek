d153ec2fa0424ed0b2076d805046cf4f2549fa46
arthursaocin <arthursaocin@gmail.com>
2024-12-31 04:00:56 +0700

    Settings Update: menambahkan tombol delete

--- Program/ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ Program/ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,7 +1,24 @@
-export const SettingsForm = () => {
-    return (
-        <div>
-            Ini Settings Form
-        </div>
-    )
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+
+interface settingPageProps {
+  initialData: Store;
 }
+
+export const SettingsForm: React.FC<settingPageProps> = ({ initialData }) => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="settings" description="Atur Toko" />
+        <Button variant="destructive" size="sm" onClick={() => {}}>
+          <Trash className="h-4 2-4" />
+        </Button>
+          </div>
+          
+    </>
+  );
+};
