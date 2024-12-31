1d902d9f9e18826a67fee61fa5dc4afd2db2d0a5
arthursaocin <arthursaocin@gmail.com>
2024-12-31 19:11:19 +0700

    API: membuat tampilan untuk api route API, copy

--- Program/ecommerce-admin/components/ui/api-alert.tsx
+++ Program/ecommerce-admin/components/ui/api-alert.tsx
@@ -24,29 +24,26 @@ export const ApiAlert: React.FC<ApiAlertProps> = ({
   description,
   variant = "public",
 }) => {
+  const onCopy = () => {
+    navigator.clipboard.writeText(description);
+    toast.success("Api berhasil di copy");
+  };
 
-    const onCopy = () => {
-        navigator.clipboard.writeText(description)
-        toast.success("Api berhasil di copy")
-    }
-
-    return (
-        <Alert>
-            <Server className="h-4 w-4" />
-            <AlertTitle>
-                {title}
-                <Badge variant={variantMap[variant]}>
-                    {textMap[variant]}
-                </Badge>
-            </AlertTitle>
-            <AlertDescription className="mt-4 flex items-center justify-between ">
-                <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold" >
-                    {description}
-                </code>
-            <Button variant={"outline"} size={"sm"} onClick={() => {}} >
-                    <Copy className="h-4 w-4" />
-                </Button>
-            </AlertDescription>
-        </Alert>
-    )
-}
+  return (
+    <Alert>
+      <Server className="h-4 w-4" />
+      <AlertTitle>
+        {title}
+        <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
+      </AlertTitle>
+      <AlertDescription className="mt-4 flex items-center justify-between ">
+        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+          {description}
+        </code>
+        <Button variant={"outline"} size={"sm"} onClick={onCopy}>
+          <Copy className="h-4 w-4" />
+        </Button>
+      </AlertDescription>
+    </Alert>
+  );
+};
