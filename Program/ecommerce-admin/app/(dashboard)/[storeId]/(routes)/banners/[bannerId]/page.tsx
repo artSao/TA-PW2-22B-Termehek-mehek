19d0416eb5eb39f94c3a0c99e346477feda413dc
arthursaocin <arthursaocin@gmail.com>
2024-12-31 20:47:54 +0700

    install next Cloudinary

--- Program/ecommerce-admin/app/(dashboard)/[storeId]/(routes)/banners/[bannerId]/page.tsx
+++ Program/ecommerce-admin/app/(dashboard)/[storeId]/(routes)/banners/[bannerId]/page.tsx
@@ -1,17 +1,18 @@
 import db from "@/lib/db";
 import { BannerForm } from "./components/banner-form";
 
-const BannerPage = async ({
-    params
-}: {
-    params: {bannerId: string };
-}) => {
+const BannerPage = async (
+    props: {
+        params: Promise<{bannerId: string }>;
+    }
+) => {
+    const params = await props.params;
     const banner = await db.banner.findUnique({
         where: {
             id: params.bannerId
         },
     });
-    
+
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
