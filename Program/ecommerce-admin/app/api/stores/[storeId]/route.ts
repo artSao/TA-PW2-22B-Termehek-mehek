3a2f5154f1f0ff1048302ea101d2bfbf9e1f4d97
arthursaocin <arthursaocin@gmail.com>
2024-12-31 18:29:03 +0700

    Settings update: membuat fungsi untuk save nama toko yang telah diubah

--- Program/ecommerce-admin/app/api/stores/[storeId]/route.ts
+++ Program/ecommerce-admin/app/api/stores/[storeId]/route.ts
@@ -3,8 +3,10 @@ import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function PATCH(
-  req: Request, 
-  { params }: {params: { storeId: string } }) {
+  req: Request,
+  props: { params: Promise<{ storeId: string }> }
+) {
+  const params = await props.params;
   try {
     const { userId } = await auth();
     const body = await req.json();
@@ -40,17 +42,17 @@ export async function PATCH(
   }
 }
 
-export async function DELETE(req: Request, props: { params: Promise<{ storeId: string }> }) {
+export async function DELETE(
+  req: Request,
+  props: { params: Promise<{ storeId: string }> }
+) {
   const params = await props.params;
   try {
     const { userId } = await auth();
-    
-
 
     if (!userId) {
       return new NextResponse("Unauthenticated", { status: 401 });
     }
-
 
     if (!params.storeId) {
       return new NextResponse("store Id dibutuhkan", { status: 400 });
@@ -61,7 +63,6 @@ export async function DELETE(req: Request, props: { params: Promise<{ storeId: s
         id: params.storeId,
         userId,
       },
-
     });
 
     return NextResponse.json(store);
