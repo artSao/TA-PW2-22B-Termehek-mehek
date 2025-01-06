a3e7f7587c12f577fa41513e85f6e429f4190693
arthursaocin <arthursaocin@gmail.com>
2025-01-06 07:01:15 +0700

    Banner Page: menambahkan menu drop down update ke page edit banner

--- Program/ecommerce-admin/app/(dashboard)/[storeId]/(routes)/banners/components/cell-action.tsx
+++ Program/ecommerce-admin/app/(dashboard)/[storeId]/(routes)/banners/components/cell-action.tsx
@@ -1,52 +1,60 @@
-'use client'
+"use client";
 
-import toast from "react-hot-toast"
+import toast from "react-hot-toast";
 
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import { BannerColumn } from "./columns"
-import { Button } from "@/components/ui/button"
-import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { BannerColumn } from "./columns";
+import { Button } from "@/components/ui/button";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+// import { useRouter } from "next/router"
+import { useParams, useRouter } from "next/navigation";
 
 interface CellActionProps {
-    data: BannerColumn
+  data: BannerColumn;
 }
 
-export const CellAction: React.FC<CellActionProps> = ({
-    data
-}) => {
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
 
-    const onCopy = (id: string) => {
-        navigator.clipboard.writeText(id);
-        toast.success("Api berhasil di copy");
-      };
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success("Api berhasil di copy");
+  };
 
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant={"ghost"}>
-                    <span className="sr-only">Open menu</span>
-                    <MoreHorizontal className="h-4 w-4" />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuLabel>
-                    Actions
-                </DropdownMenuLabel>
-                <DropdownMenuItem onClick={() => onCopy(data.id)}>
-                    <Copy className="mr-2 h-4 w-4" />
-                    Copy ID
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <Edit className="mr-2 h-4 w-4" />
-                    Update ID
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <Trash className="mr-2 h-4 w-4" />
-                    Delete ID
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
-}
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"ghost"}>
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem onClick={() => onCopy(data.id)}>
+          <Copy className="mr-2 h-4 w-4" />
+          Copy ID
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => router.push(`/${params.storeId}/banners/${data.id}`)}
+        >
+          <Edit className="mr-2 h-4 w-4" />
+          Update
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Trash className="mr-2 h-4 w-4" />
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 // export const cellAction()
