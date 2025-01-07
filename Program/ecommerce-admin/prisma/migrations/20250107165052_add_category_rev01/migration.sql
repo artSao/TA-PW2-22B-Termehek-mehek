eaa9705ed5639339f18eafbadcca3734b9b07df8
arthursaocin <arthursaocin@gmail.com>
2025-01-07 23:52:40 +0700

    schema prisma: membuat table Category

---
+++ Program/ecommerce-admin/prisma/migrations/20250107165052_add_category_rev01/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "storeId" TEXT NOT NULL,
+    "bannerId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Category_storeId_idx" ON "Category"("storeId");
+
+-- CreateIndex
+CREATE INDEX "Category_bannerId_idx" ON "Category"("bannerId");
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "Store"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_bannerId_fkey" FOREIGN KEY ("bannerId") REFERENCES "Banner"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
