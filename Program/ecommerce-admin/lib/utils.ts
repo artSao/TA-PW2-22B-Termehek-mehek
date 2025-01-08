7c2039d420cead54fa69c10540c870d0646470f6
arthursaocin <arthursaocin@gmail.com>
2025-01-08 20:38:31 +0700

    membuat currency untuk harga

--- Program/ecommerce-admin/lib/utils.ts
+++ Program/ecommerce-admin/lib/utils.ts
@@ -1,6 +1,14 @@
 import { clsx, type ClassValue } from "clsx"
+import { Currency } from "lucide-react"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const formatter = new Intl.NumberFormat("id-ID", {
+  minimumFractionDigits: 0,
+  style: "currency",
+  currency: "IDR",
+})
