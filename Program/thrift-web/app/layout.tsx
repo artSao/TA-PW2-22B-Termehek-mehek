fc9b2ef45d1841f81ae40f7657f3d323ddf0afc2
tommyorizchotambun <tommy_orizcho_tambun@teknokrat.ac.id>
2025-01-21 22:36:23 +0700

    update menambahkan footer,container,dan navbar

--- Program/thrift-web/app/layout.tsx
+++ Program/thrift-web/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
 
 const font = Urbanist({
   subsets: ["latin"],
@@ -19,9 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${font.className} ${font.className} antialiased`}
-      >
+        className={font.className}>
+          <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
