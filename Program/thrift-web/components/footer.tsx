c5f95f8997bbf1f778fd1d6862e0b682e74a25cb
arthursaocin <arthursaocin@gmail.com>
2025-01-21 22:55:29 +0700

    update

--- Program/thrift-web/components/footer.tsx
+++ Program/thrift-web/components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
     <footer className="bg-white border-t">
         <div className="mx-auto py=10">
-            <p className="text-center text -xs text-black ">&copy; Thrift Web  </p>
+            <p className="text-center text -xs text-black ">&copy; 2024 Thrift Web, Inc. All rights reserved </p>
         </div>
     
     </footer>
